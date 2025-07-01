54d9fcad96b9a0237736bc3d5b51590d1233fc0d
Garvit Rakhecha <garvitrakhecha786@gmail.com>
2025-07-01 10:17:38 +0530

    Add initial TypeScript and HTML files with basic structure

---
+++ L1/app.ts
@@ -0,0 +1,14 @@
+// Number , string, boolean
+function add(num1:number, num2:number, num3:boolean, someText:string): number | boolean {
+    if(num3){
+        console.log(`${someText}, ${num1 + num2}`);
+         
+    }
+    return num3
+}
+
+const num1 = 5
+const num2 = 5
+const printNumber = true
+const someText = "this is some text"
+const num3: boolean | number | string = add(num1, num2, printNumber, someText)
